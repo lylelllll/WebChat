a9b5ebfe0b620fdb040c926f485bec4658e115c7
lylelllll <1179454895@qq.com>
2022-04-22 16:39:53 +0800

    Account View

--- ChatServerPlayground/settings.py
+++ ChatServerPlayground/settings.py
@@ -151,5 +151,5 @@ MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static_cdn')
 MEDIA_ROOT = os.path.join(BASE_DIR,'media_cdn')
 TEMP = os.path.join(BASE_DIR,'media_cdn/temp')
-BASE_DIR = "http://127.0.0.1:8000"
+BASE_URL = "http://127.0.0.1:8000"
 
