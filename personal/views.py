49fa7a7cfdc498027150ccdba79fde632d09cb10
lyle <1179454895@qq.com>
2022-05-06 11:13:34 +0800

    query history chat_message and paginator

--- personal/views.py
+++ personal/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.conf import settings
-# Create your views here.
 
-def home_screen_view(request, *args, **kwargs):
-    context = {'debug_mode': settings.DEBUG, 'room_id': "2"}
+DEBUG = True
 
-    return render(request,"personal/home.html",context)
+def home_screen_view(request):
+	context = {}
+	context['debug_mode'] = settings.DEBUG
+	context['debug'] = DEBUG
+	context['room_id'] = "2"
+	return render(request, "personal/home.html", context)
